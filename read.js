43d9e397c04870de59c4ed35a8ce8c29072f3e43
Alex Ono <work.alexanderono@yahoo.com>
2021-06-01 13:25:38 +0100

    Graph with studies completed per day
    Messy code, needs refactoring, but it works. Maybe possible to add some buttons that control how much you see on the graph as currently is quite squashed

--- read.js
+++ read.js
@@ -2,11 +2,10 @@ const reader = new FileReader()
 let highest_bonus = 0;
 let highest_payout = 0;
 let studies = [];
+let study_count = [];
 let selector = 0;
 let start = 67;
 let count = 0;
-let studies_array_iter = 0;
-let double_quotes = 0;
 let ignore_commas = false;
 let study_name = "";
 let study_pay = "";
@@ -18,6 +17,10 @@ let study_result = "";
 let studies_rejected = 0;
 let studies_timed_out = 0;
 let studies_returned = 0;
+let study_labels = [];
+let study_dataset = [];
+let loading_done = false;
+let yyyy = "", mm = "", dd = "";
 
 function read(input) {
 	const csv = input.files[0]
@@ -28,7 +31,7 @@ reader.onload = function (e) {
     let long_string = e.target.result;
 	while(start != long_string.length){
 		if(long_string.charAt(start + count) == "\""){
-			console.log("Found a double quote");
+			// console.log("Found a double quote");
 			ignore_commas = !ignore_commas;
 		}
 		if(long_string.charAt(start + count) == ',' && !ignore_commas){
@@ -67,9 +70,31 @@ reader.onload = function (e) {
 				}
 				studies.push([study_name, study_pay, study_bonus, study_start, study_finish,
 				study_code, study_result]);
+				if(study_result = 'APPROVED'){
+					if(study_count.length == 0){
+						study_count.push([study_start, 1]);
+						// console.log(study_count[0][0]);
+						// console.log(study_count[0][1]);
+					} else {
+						// console.log("starting for loop for study start checks");
+						let found = false;
+						for (i = 0 ; i < study_count.length ; i++){
+							if( study_count[i][0].toDateString() == study_start.toDateString()){
+								// console.log("Found a match, incrementing");
+								study_count[i][1] += 1;
+								found = true;
+							}
+						}
+						if(found == false){
+							// console.log("No match, pushing to array");
+							study_count.push([study_start, 1]);
+						}
+						// console.log(found);
+					}
+				}
 				// console.log('console logging array contents...');
-				count = studies.length - 1;
-				console.log(studies[count]);
+				// count = studies.length - 1;
+				// console.log(studies[count]);
 				count = 0;
 				selector = 0;
 			} else {
@@ -89,6 +114,12 @@ reader.onload = function (e) {
 						break;
 					case 3:
 						study_start = long_string.substr(start, count);
+						study_start = study_start.substr(0, 10);
+						study_start = new Date(study_start);
+						//let yyyy = study_start.substr(0, 4);
+						//let mm = study_start.substr(5, 2);
+						//let dd = study_start.substr(8, 2);
+						//study_start = new Date(yyyy, mm, dd);
 						break;
 					case 4:
 						study_finish = long_string.substr(start, count);
@@ -104,11 +135,24 @@ reader.onload = function (e) {
 			count +=1;
 		}
 	}
-	document.querySelector('.output').innerText = `Highest payout = ${highest_payout / 100},
-	 Highest bonus = ${highest_bonus / 100},
+	document.querySelector('.output').innerText = `Highest ever payout = ${highest_payout / 100},
+	 Highest ever bonus = ${highest_bonus / 100},
 	 Studies returned = ${studies_returned},
 	 Studies rejected by researcher = ${studies_rejected},
 	 Studies timed out of = ${studies_timed_out}`;
+	 console.log(study_count);
+	 console.log(studies);
+	 study_count.sort(function(a,b){
+	 return a[0] - b[0];
+	 });
+	 for (i = 0 ; i < study_count.length ; i++){
+		study_labels.push(study_count[i][0].toDateString());
+		study_dataset.push(study_count[i][1]);
+
+	}
+	console.log(study_labels);
+	console.log(study_dataset);
+	load_graph();
 }
 
 /* 67 is the index of the first character of the first study
